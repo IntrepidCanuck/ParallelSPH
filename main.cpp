6d1d08224dfbd9cec1a9bb930e5207ec6b93c924
Stuart Park <stuartpark@Stuarts-Air.hitronhub.home>
2021-12-09 15:20:05 -0500

    Added loop and density/pressure calculations

--- main.cpp
+++ main.cpp
@@ -7,10 +7,46 @@
 
 using namespace std;
 
+double kernelCalc(double x1, double x2, double y1, double y2, double rad){
+  double dist = (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))/rad;
+
+  if (dist < 1.0){
+    return (4.0/3.14159/pow(rad,2))*pow(1-pow(dist,2),3);
+  } else {
+    return 0.0;
+  }
+}
+
+double FirstOrderKernelCalcX(double x1, double x2, double y1, double y2, double rad){
+  double dist = (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))/rad;
+
+  if (dist < 1.0){
+    return (-30/3.14159/pow(rad,3))*(x1-x2)*(pow(1-dist,2)/dist);
+  } else {
+    return 0.0;
+  }
+}
+
+double FirstOrderKernelCalcY(double x1, double x2, double y1, double y2, double rad){
+  double dist = (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))/rad;
+
+  if (dist < 1.0){
+    return (-30/3.14159/pow(rad,3))*(y1-y2)*(pow(1-dist,2)/dist);
+  } else {
+    return 0.0;
+  }
+}
+
 int main(int argc, char **argv){
 
 // Initialize parameters of the problem which can be defined by the user
-int N = 100; // Number of particles
+int N = 900; // Number of particles
+int mass = 1; // Initial guess for mass (will be updated later)
+double radInfluence = 1; // Radius of Influence of each particle
+double restingDensity = 1000; // Resting density of the fluid
+double T = 10; // Total time of simulation
+double dt = 0.01; // Time step
+double pressureConstant = 2000; // Pressure constant
 
 // Do not allow the number of particles to be anything other than a perfect square
 // for simplicity
@@ -21,6 +57,8 @@ if (ceil((double)sqrt(N)) != floor((double)sqrt(N))){
 // Define arrays to store properties of each particle at each time step
 double* density = new double[N];
 double* pressure = new double[N];
+double* pressureX = new double[N]; // Pressure forces in the x and y direction
+double* pressureY = new double[N];
 double* x = new double[N]; // x and y location
 double* y = new double[N];
 double* u = new double[N]; // u and v velocity components
@@ -29,6 +67,7 @@ double* v = new double[N];
 // Initialize the particles at the center of the domain
 int linearN = sqrt(N);
 double dx = 0.5/(double)linearN; // Calculate the spacing between each particle
+printf("dx = %f\n",dx);
 // Loop through every particle and calculate the starting position of each particle within
 // the centered 0.5x0.5 box
 for (int i = 0;i < N; ++i){
@@ -36,9 +75,68 @@ for (int i = 0;i < N; ++i){
   y[i] = ((i/linearN)/(double)(linearN-1)*0.5)+0.25;
 }
 
+// We first need to determine the mass which each particle represents
+double densityContribution;
+for (int i = 0; i < N; ++i){
+  densityContribution = 0.0;
+  for (int j = 0; j < N; ++j){
+    densityContribution += mass*kernelCalc(x[i],y[i],x[j],y[j],radInfluence);
+  }
+  density[i] = densityContribution;
+}
+
+// Now recalculate the mass, assuming mass is spread evenly among all particles
+double densitySum = 0;
+for (int i = 0; i < N; ++i){
+  densitySum += density[i];
+}
+mass = ((double)N*restingDensity)/densitySum;
+
+// Counter for the current timestep of the simulation
+currentTime = 0.0;
+
+while (currentTime <= T){
+
+  // Perform the density calculation
+  for (int i = 0; i < N; ++i){
+    densityContribution = 0.0;
+    for (int j = 0; j < N; ++j){
+      densityContribution += mass*kernelCalc(x[i],y[i],x[j],y[j],radInfluence);
+    }
+    density[i] = densityContribution;
+  }
+
+  // Perform the Pressure calculation
+  pressure[i] = pressureConstant*(density[i] - restingDensity);
+
+  // Perform the Pressure force Calculation
+  double pressureContributionX;
+  double pressureContributionY;
+  for (int i = 0; i < N; ++i){
+    pressureContributionX = 0.0;
+    pressureContributionY = 0.0;
+    for (int j = 0; j < N, ++j){
+      pressureContributionX += (mass/density[j])*(pressure[i]+pressure[j])*(1/2)*FirstOrderKernelCalcX(x[i],y[i],x[j],y[j],radInfluence);
+      pressureContributionY += (mass/density[j])*(pressure[i]+pressure[j])*(1/2)*FirstOrderKernelCalcX(x[i],y[i],x[j],y[j],radInfluence);
+    }
+    pressureX[i] = -1.0*pressureContributionX;
+    pressureY[i] = -1.0*pressureContributionY;
+  }
 
 
-printf("%f",dx);
+
+
+
+
+
+
+
+}
+
+
+
+
+
 
 
 }
