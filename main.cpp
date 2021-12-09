e6bb73aeed87d65ab950146f7c2b1613350ee02a
Stuart Park <stuartpark@Stuarts-Air.hitronhub.home>
2021-12-09 12:52:28 -0800

    Added visc calculation, force calculation, and loop.  Program now outputs something but don't know if it is correct yet.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <math.h>
 
 // This is a program to perform a relatively simple SPH fluid particle simulation
@@ -7,7 +8,7 @@
 
 using namespace std;
 
-double kernelCalc(double x1, double x2, double y1, double y2, double rad){
+double kernelCalc(double x1, double y1, double x2, double y2, double rad){
   double dist = (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))/rad;
 
   if (dist < 1.0){
@@ -17,7 +18,7 @@ double kernelCalc(double x1, double x2, double y1, double y2, double rad){
   }
 }
 
-double FirstOrderKernelCalcX(double x1, double x2, double y1, double y2, double rad){
+double FirstOrderKernelCalcX(double x1, double y1, double x2, double y2, double rad){
   double dist = (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))/rad;
 
   if (dist < 1.0){
@@ -27,7 +28,7 @@ double FirstOrderKernelCalcX(double x1, double x2, double y1, double y2, double 
   }
 }
 
-double FirstOrderKernelCalcY(double x1, double x2, double y1, double y2, double rad){
+double FirstOrderKernelCalcY(double x1, double y1, double x2, double y2, double rad){
   double dist = (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))/rad;
 
   if (dist < 1.0){
@@ -37,16 +38,43 @@ double FirstOrderKernelCalcY(double x1, double x2, double y1, double y2, double 
   }
 }
 
+double SecondOrderKernelCalcX(double x1, double y1, double x2, double y2, double u1, double u2, double rad){
+  double dist = (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))/rad;
+
+  if (dist < 1.0){
+    return (12/pow(rad,4))*(u1 - u2)*(1-dist);
+  } else {
+    return 0.0;
+  }
+}
+
+double SecondOrderKernelCalcY(double x1, double y1, double x2, double y2, double v1, double v2, double rad){
+  double dist = (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))/rad;
+
+  if (dist < 1.0){
+    return (12/pow(rad,4))*(v1-v2)*(1-dist);
+  } else {
+    return 0.0;
+  }
+}
+
 int main(int argc, char **argv){
+
+// Open up a file to which we can output data
+string file_name = "jacobi_" + to_string(rank) + ".txt";
+ofstream fp(file_name);
 
 // Initialize parameters of the problem which can be defined by the user
 int N = 900; // Number of particles
 int mass = 1; // Initial guess for mass (will be updated later)
 double radInfluence = 1; // Radius of Influence of each particle
 double restingDensity = 1000; // Resting density of the fluid
-double T = 10; // Total time of simulation
+double T = 1; // Total time of simulation
 double dt = 0.01; // Time step
 double pressureConstant = 2000; // Pressure constant
+double viscConstant = 1.0; // Viscosity Constant
+double gravConstant = 9.8; // Gravitational Constant
+double restitutionConst = 0.5; // Restitution Coeffecient
 
 // Do not allow the number of particles to be anything other than a perfect square
 // for simplicity
@@ -59,6 +87,9 @@ double* density = new double[N];
 double* pressure = new double[N];
 double* pressureX = new double[N]; // Pressure forces in the x and y direction
 double* pressureY = new double[N];
+double* viscosityX = new double[N]; // Viscosity forces in the x and y direction
+double* viscosityY = new double[N];
+double* gravY = new double[N]; // Gravity force
 double* x = new double[N]; // x and y location
 double* y = new double[N];
 double* u = new double[N]; // u and v velocity components
@@ -73,6 +104,12 @@ printf("dx = %f\n",dx);
 for (int i = 0;i < N; ++i){
   x[i] = ((i%linearN)/(double)(linearN-1)*0.5)+0.25;
   y[i] = ((i/linearN)/(double)(linearN-1)*0.5)+0.25;
+}
+
+// Initialize the velocities as stationary
+for (int i = 0; i < N; ++i){
+  u[i] = 0.0;
+  v[i] = 0.0;
 }
 
 // We first need to determine the mass which each particle represents
@@ -93,9 +130,11 @@ for (int i = 0; i < N; ++i){
 mass = ((double)N*restingDensity)/densitySum;
 
 // Counter for the current timestep of the simulation
-currentTime = 0.0;
+double currentTime = 0.0;
 
 while (currentTime <= T){
+
+  printf("%f \n",currentTime);
 
   // Perform the density calculation
   for (int i = 0; i < N; ++i){
@@ -107,7 +146,9 @@ while (currentTime <= T){
   }
 
   // Perform the Pressure calculation
-  pressure[i] = pressureConstant*(density[i] - restingDensity);
+  for (int i = 0; i < N; ++i){
+    pressure[i] = pressureConstant*(density[i] - restingDensity);
+  }
 
   // Perform the Pressure force Calculation
   double pressureContributionX;
@@ -115,7 +156,7 @@ while (currentTime <= T){
   for (int i = 0; i < N; ++i){
     pressureContributionX = 0.0;
     pressureContributionY = 0.0;
-    for (int j = 0; j < N, ++j){
+    for (int j = 0; j < N; ++j){
       pressureContributionX += (mass/density[j])*(pressure[i]+pressure[j])*(1/2)*FirstOrderKernelCalcX(x[i],y[i],x[j],y[j],radInfluence);
       pressureContributionY += (mass/density[j])*(pressure[i]+pressure[j])*(1/2)*FirstOrderKernelCalcX(x[i],y[i],x[j],y[j],radInfluence);
     }
@@ -123,9 +164,67 @@ while (currentTime <= T){
     pressureY[i] = -1.0*pressureContributionY;
   }
 
+  // Perform the viscous force calculations
+  double viscousContributionX;
+  double viscousContributionY;
+  for (int i = 0; i < N; ++i){
+    viscousContributionX = 0.0;
+    viscousContributionY = 0.0;
+    for (int j = 0; j < N; ++j){
+      viscousContributionX += (mass/density[j])*SecondOrderKernelCalcX(x[i],y[i],x[j],y[j],u[i],u[j],radInfluence);
+      viscousContributionY += (mass/density[j])*SecondOrderKernelCalcY(x[i],y[i],x[j],y[j],v[i],v[j],radInfluence);
+    }
+    viscosityX[i] = -1.0*viscConstant*viscousContributionX;
+    viscosityY[i] = -1.0*viscConstant*viscousContributionY;
+  }
+
+  // Perform the gravity force calculations
+  for (int i = 0; i < N; ++i){
+    gravY[i] = -1.0*density[i]*gravConstant;
+  }
+
+  // Update the velocity
+  for (int i = 0; i < N; ++i){
+    u[i] += (pressureX[i]+viscosityX[i])*dt/density[i];
+    v[i] += (pressureY[i]+viscosityY[i]+gravY[i])*dt/density[i];
+  }
+
+  // Update the position of each particle
+  for (int i = 0; i < N; ++i){
+    x[i] += u[i]*dt;
+    y[i] += v[i];
+  }
+
+  // Check for the boundary Conditions
+  for (int i = 0; i < N; ++i){
+    // Check bottom boundary condition
+    if (y[i] <= radInfluence){
+      y[i] = radInfluence;
+      v[i] = -1.0*restitutionConst*v[i];
+    }
+
+    // Check top boundary Conditions
+    if  (y[i] >= 1-radInfluence){
+      y[i] = 1 - radInfluence;
+      v[i] = -1.0*restitutionConst*v[i];
+    }
+
+    // Check left boundary conditions
+    if (x[i] <= radInfluence){
+      x[i] = radInfluence;
+      u[i] = -1.0*restitutionConst*u[i];
+    }
+
+    // Check right boundary conditions
+    if (x[i] >= 1-radInfluence){
+      x[i] = 1-radInfluence;
+      u[i] = -1.0*restitutionConst*u[i];
+    }
+  }
 
 
-
+// Increment the counter
+currentTime += dt;
 
 
 
