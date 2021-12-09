9cec6e4865caa6f7134893bb5256795ad2ae42a8
Stuart Park <stuartpark@Stuarts-Air.hitronhub.home>
2021-12-09 11:47:21 -0500

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// This is a program to perform a relatively simple SPH fluid particle simulation
+// in a Parallelized manner.  The bulk of the simulation is carried out within the simulation.cpp
+// file, while the main.cpp file is the caller file controls and configures the simulation
+
+using namespace std
+
+int main(int argc, char **argv){
+
+
+
+
+
+}
